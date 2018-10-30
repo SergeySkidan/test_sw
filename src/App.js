03ddb2394128d46e893c127974716d9e95d9652c
SergeySkidan <SergeySkidan1982@gmail.com>
2018-10-30 20:08:28 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import banner from './images/banner.png';
+import Logo from './images/Logo.png';
+import menu from './images/menu.png';
+import to_order from './images/to_order.png';
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+                <header className="header">
+                       <img src={menu} className="line_block" alt="menu" />
+                       <img src={Logo} className="line_block" alt="Logo" />
+                       <img src={to_order} className="line_block" alt="to_order" />
+                </header>
+
+
+             <main>
+
+                 <img src={banner} className="banner" alt="banner" />
+
+            </main>
+
+            <footer>
+                <div>
+                    dfsfsdfd
+                </div>
+
+            </footer>
+
+
+
+
       </div>
     );
   }
