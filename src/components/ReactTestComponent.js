89b3dfcac52bc1cc2a1b1700a1982edc1b6811cc
SergeySkidan <SergeySkidan1982@gmail.com>
2018-11-15 11:16:52 +0200

    commit

---
+++ src/components/ReactTestComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ReactTestComponent extends React.Component{
+    render(){
+        return(
+            <div className={"react-test-component"}>
+
+                <h1>DDDDDDDDDDDDDDDDDDDDDDD</h1>
+
+
+            </div>
+        )
+    }
+}
